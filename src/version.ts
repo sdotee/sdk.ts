684c01df3f0effe317be80794d229c82c5ef1d8a
mingcheng <mingcheng@apache.org>
2025-12-04 17:10:16 +0800

    feat: add copyright headers and update version to 1.0.0 in source files

--- src/version.ts
+++ src/version.ts
@@ -1,2 +1,16 @@
-export const Version = "0.1.0";
-export const UserAgent = `SeeUrlShortener-SDK/${Version}`;
+/*!*
+ * Copyright (c) 2025 S.EE Development Team
+ *
+ * This source code is licensed under the MIT License,
+ * which is located in the LICENSE file in the source tree's root directory.
+ *
+ * File: version.ts
+ * Author: S.EE Development Team <dev@s.ee>
+ * File Created: 2025-07-03 16:15:36
+ *
+ * Modified By: S.EE Development Team <dev@s.ee>
+ * Last Modified: 2025-12-04 17:08:59
+ */
+
+export const Version = "1.0.0";
+export const UserAgent = `See-SDK/${Version}`;
