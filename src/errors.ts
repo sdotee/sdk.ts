669ffd7c4f8ebc49a5241a866cec0e8203676569
mingcheng <mingcheng@apache.org>
2025-12-04 17:10:16 +0800

    feat: add copyright headers and update version to 1.0.0 in source files

--- src/errors.ts
+++ src/errors.ts
@@ -1,3 +1,17 @@
+/*!*
+ * Copyright (c) 2025 S.EE Development Team
+ *
+ * This source code is licensed under the MIT License,
+ * which is located in the LICENSE file in the source tree's root directory.
+ *
+ * File: errors.ts
+ * Author: S.EE Development Team <dev@s.ee>
+ * File Created: 2025-07-03 11:54:40
+ *
+ * Modified By: S.EE Development Team <dev@s.ee>
+ * Last Modified: 2025-12-04 17:10:04
+ */
+
 import {ApiError} from './types';
 
 export class UrlShortenerError extends Error {
