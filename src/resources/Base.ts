b8909433984c5a29a3c7a21518d062a2ec60a672
mingcheng <mingcheng@apache.org>
2026-01-20 17:24:44 +0800

    refactor: change imports to use 'type' for type-only imports in resource files and tests

--- src/resources/Base.ts
+++ src/resources/Base.ts
@@ -8,7 +8,7 @@
  * Author: S.EE Development Team <dev@s.ee>
  */
 
-import { AxiosInstance } from "axios";
+import type { AxiosInstance } from "axios";
 
 export abstract class BaseResource {
     protected client: AxiosInstance;
